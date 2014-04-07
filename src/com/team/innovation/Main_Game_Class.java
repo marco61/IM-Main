42a3692f51eae5af77d78500e27fea0085ba44da
marco61 <marco61@iastate.edu>
2014-04-06 21:30:46 -0500

    g
    fg

--- src/com/team/innovation/Main_Game_Class.java
+++ src/com/team/innovation/Main_Game_Class.java
@@ -8,22 +8,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Main_Game_Class implements ApplicationListener {
-
+	
 	SpriteBatch batch;
-<<<<<<< HEAD
 	Texture mT,texture;
 	Player mP;	
-=======
-	Texture mT;
-	Player mP;
->>>>>>> 566ad35d0cfc3ed8047f92cf0d1be7f7dda37941
 	Vector2 position;
-	Texture texture;
-
+	
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-<<<<<<< HEAD
 		mP = new Player(new Vector2(0, 0), "data/planeRed1.png");
 		texture = new Texture(Gdx.files.internal("data/pppp.png"));
 		
@@ -31,53 +24,35 @@ public class Main_Game_Class implements ApplicationListener {
 				
 			
 		
-=======
-		mT = new Texture(Gdx.files.internal("data/testSS.png"));
-		mP = new Player(new Vector2(10, 300), "data/mT.jpg");
-		texture = new Texture(Gdx.files.internal("data/pppp.png"));
-	}
->>>>>>> 566ad35d0cfc3ed8047f92cf0d1be7f7dda37941
 
 	@Override
 	public void dispose() {
-
+		
 		batch.dispose();
-
+		
 		mT.dispose();
-
+			
 	}
 
 	@Override
-<<<<<<< HEAD
 	public void render() {		
-=======
-	public void render() {
 
-		
->>>>>>> 566ad35d0cfc3ed8047f92cf0d1be7f7dda37941
-
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT); 
+	     
 		batch.begin();
-
+	     
 		batch.draw(texture, 0, 0);
-
+	     
 		batch.end();
-<<<<<<< HEAD
 		
-=======
-
-		mT.dispose();
-
->>>>>>> 566ad35d0cfc3ed8047f92cf0d1be7f7dda37941
 		mP.update();
-
+		
 		batch.begin();
-
-		batch.draw(mP.getCurrentFrame(), mP.getPosition().x, mP.getPosition().y);
-
+		
+		batch.draw(mP.getCurrentFrame(), mP.getPosition().x , mP.getPosition().y);
+		
 		batch.end();
-
+		
 	}
 
 	@Override
