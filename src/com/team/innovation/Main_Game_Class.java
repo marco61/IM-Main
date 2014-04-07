566ad35d0cfc3ed8047f92cf0d1be7f7dda37941
Keane O'Kelley <keane.m.okelley@gmail.com>
2014-04-06 21:08:26 -0500

    Fix crazy memory leak!

--- src/com/team/innovation/Main_Game_Class.java
+++ src/com/team/innovation/Main_Game_Class.java
@@ -8,55 +8,53 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Main_Game_Class implements ApplicationListener {
-	
+
 	SpriteBatch batch;
 	Texture mT;
-	Player mP;	
+	Player mP;
 	Vector2 position;
-	
+	Texture texture;
+
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
 		mT = new Texture(Gdx.files.internal("data/testSS.png"));
 		mP = new Player(new Vector2(10, 300), "data/mT.jpg");
-		
-		}
-				
-			
-		
-
-	@Override
-	public void dispose() {
-		
-		batch.dispose();
-		
-		mT.dispose();
-			
+		texture = new Texture(Gdx.files.internal("data/pppp.png"));
 	}
 
 	@Override
-	public void render() {		
-		
-		Texture texture = new Texture(Gdx.files.internal("data/pppp.png"));
+	public void dispose() {
 
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT); 
-	     
-		batch.begin();
-	     
-		batch.draw(texture, 0, 0);
-	     
-		batch.end();
-		
+		batch.dispose();
+
 		mT.dispose();
+
+	}
+
+	@Override
+	public void render() {
+
 		
-		mP.update();
-		
+
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		
-		batch.draw(mP.getCurrentFrame(), mP.getPosition().x , mP.getPosition().y);
-		
+
+		batch.draw(texture, 0, 0);
+
 		batch.end();
-		
+
+		mT.dispose();
+
+		mP.update();
+
+		batch.begin();
+
+		batch.draw(mP.getCurrentFrame(), mP.getPosition().x, mP.getPosition().y);
+
+		batch.end();
+
 	}
 
 	@Override
