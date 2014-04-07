0d478926b62c6c8e5db133c69a2dd463aa7e3f1b
Keane O'Kelley <keane.m.okelley@gmail.com>
2014-04-06 20:30:10 -0500

    Implement class to render the world.

--- src/com/team/innovation/WorldRender.java
+++ src/com/team/innovation/WorldRender.java
@@ -1,5 +1,6 @@
 package com.team.innovation;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
@@ -25,6 +26,10 @@ public class WorldRender {
 		for (Obstacle obstacle : world.getArray()) {
 			Rectangle rectangle = obstacle.getBounds();
 			float x1 = obstacle.getPosition().x + rectangle.x;
+			float y1 = obstacle.getPosition().y + rectangle.y;
+			debugRenderer.setColor(new Color(1, 0, 0, 1));
+			debugRenderer.rect(x1, y1, rectangle.width, rectangle.height);
+			debugRenderer.end();
 		}
 	}
 
