4b66373105075a44a66bc1acb3a61de6244929b4
Keane O'Kelley <keane.m.okelley@gmail.com>
2014-04-06 19:14:05 -0500

    Add some texture code. Needs actual assets

--- src/com/team/innovation/Obstacle.java
+++ src/com/team/innovation/Obstacle.java
@@ -1,5 +1,8 @@
 package com.team.innovation;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
@@ -8,8 +11,15 @@ public class Obstacle {
 	static final float SIZE = 1f;
 	Vector2 position = new Vector2();
 	Rectangle rectangle = new Rectangle();
+	Texture obstacleTexture;
+	TextureRegion[] frames;
+	TextureRegion currentFrame;
 	
 	public Obstacle(Vector2 position) {
+		obstacleTexture = new Texture(Gdx.files.internal("data/sw.png")); //needs actual asset
+		TextureRegion[][] tmp = TextureRegion
+				.split(obstacleTexture, obstacleTexture.getWidth(),
+						obstacleTexture.getHeight());
 		this.position = position;
 		this.rectangle.width = SIZE;
 		this.rectangle.height = SIZE;
