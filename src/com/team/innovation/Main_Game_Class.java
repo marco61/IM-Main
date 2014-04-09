5f20cd5688692140df83e49251331c2072c4a414
marco61 <marco61@iastate.edu>
2014-04-09 13:11:23 -0500

    backround change

--- src/com/team/innovation/Main_Game_Class.java
+++ src/com/team/innovation/Main_Game_Class.java
@@ -3,6 +3,7 @@ package com.team.innovation;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -13,12 +14,14 @@ public class Main_Game_Class implements ApplicationListener {
 	Texture mT,texture;
 	Player mP;	
 	Vector2 position;
+	OrthographicCamera camera;
 	
 	@Override
 	public void create() {
+		camera = new OrthographicCamera(1280, 720);
 		batch = new SpriteBatch();
 		mP = new Player(new Vector2(0, 0), "data/planeRed1.png");
-		texture = new Texture(Gdx.files.internal("data/pppp.png"));
+		texture = new Texture(Gdx.files.internal("data/BB.png"));
 		
 		}
 				
@@ -38,6 +41,8 @@ public class Main_Game_Class implements ApplicationListener {
 	public void render() {		
 
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT); 
+		
+		batch.setProjectionMatrix(camera.combined);
 	     
 		batch.begin();
 	     
@@ -65,5 +70,15 @@ public class Main_Game_Class implements ApplicationListener {
 
 	@Override
 	public void resume() {
+	}
+	
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		   
+		camera.translate(deltaX,0);
+		  
+		camera.update();
+		  
+		return false;
+		
 	}
 }
