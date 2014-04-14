aa069b338a6f6a297111e4e564731f005f7a2654
marco61 <marco61@iastate.edu>
2014-04-13 20:09:06 -0500

    fix setup

--- src/com/team/innovation/Main_Game_Class.java
+++ src/com/team/innovation/Main_Game_Class.java
@@ -2,7 +2,7 @@ package com.team.innovation;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -42,7 +42,7 @@ public class Main_Game_Class implements ApplicationListener {
 	@Override
 	public void render() {
 
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.setProjectionMatrix(camera.combined);
 
